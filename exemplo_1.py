3f229d745d6037b9fd0c1a092d1ca23848c5d393
Filipe Cifali Stangler <cifali@kinghost.com.br>
2017-05-29 16:38:04 -0300

    Tentando formatar melhor

--- exemplo_1.py
+++ exemplo_1.py
@@ -6,9 +6,9 @@ def tipos_de_dados():
     >>> lista.append(4)
     >>> print(lista)
     [1, 2, 3, 4]
-    >>> tupla = ('jan', 'fev', 'mar', 'abr', 'mai', 'jun',
-            'jul', 'ago', 'set', 'out', 'nov', 'dez') # Tuplas sao imutaveis,
-    >>> # uteis para dados estaticos mas nem sempre mais rapidas
+    >>> tupla = ('jan', 'fev', 'mar', 'abr', 'mai', 'jun', \
+'jul', 'ago', 'set', 'out', 'nov', 'dez') # Tuplas sao imutaveis \
+uteis para dados estaticos mas nem sempre mais rapidas
     >>> dicts = {hero='Nyx', role='Gankerino', kda='4.2'}
     >>> # Dicionarios sao interessantes pois podem receber dados de maneira serializada
     """
